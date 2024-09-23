496825ac12a681560904a9fc636254999135b777
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:03:06 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -4,5 +4,6 @@ x = st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write("Anda memasukka", x, ' ', sx)
 sy = st.text_input("Dikonversi ke", "F")
-
-st.write (x, ' ', sx, '=...', sy)
+if(konversi == "F"):
+  y = x*4/5
+st.write (y, ' ', sx, '=...', sy)
