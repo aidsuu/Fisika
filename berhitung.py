75e5fe15ec53bac9c92de8ae9bbf0e778597e707
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:28:21 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
+sx = st.selectbox("sx:", "C", "F")
 sx = st.text_input("Satuan", "C")
 st.write("Anda memasukka", x, ' ', sx)
 sy = st.text_input("Dikonversi ke", "F")
@@ -12,8 +13,4 @@ if(sx == "C"):
     y = x*9/5+32
 st.write (x, ' ', sx, ' = ',y, sy)
 
-with st.popover("Konversi Suhu"):
-    st.markdown("Konversi Suhu 👋")
-    name = st.text_input("Ingin Konversi apa?")
 
-st.write("Your name:", name)
