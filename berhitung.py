4978f5d555b12ca05813287d9a0f9877115bcb98
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:36:45 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -4,10 +4,15 @@ x = st.number_input("Masukkan angka")
 sx = st.text_input("Satuan", "C")
 st.write("Anda memasukka", x, ' ', sx)
 sy = st.text_input("Dikonversi ke", "F")
+sz = st.text_input("Dikonversi ke", "K")
 y = 0
+z = 0
 if(sx == "C"):
   if(sy == 'C'):
     y = x
   elif(sy == 'F'):
     y = x*9/5+32
+  elif(sz == 'K'):
+    y = x+273.15
 st.write (x, ' ', sx, ' = ',y, sy)
+st.write (x, ' ', sx, ' = ',z, sz)
