a5e71200f6894293ec22dceba364e5c539539ec8
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 09:51:08 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-x = st.number_input(
-  "Insert a number", value=none, placeholder="Type a number..."
-)
-st.writer("The current number is ", x)
-st.later(r'''
-x^2 =
-''')
-st.write(x*x)
+x = st.number_input("Masukkan angka")
+sx = st.text_input("Satuan", "C")
+st.write("Anda memasukka", x, ' ', sx)
