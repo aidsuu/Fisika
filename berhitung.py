14ab19b97670c7fb701061162c16a2366554140e
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:42:08 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -6,17 +6,14 @@ st.write("Anda memasukka", x, ' ', sx)
 sy = st.text_input("Dikonversi ke", "F")
 sz = st.text_input("Dikonversi ke", "K")
 y = 0
+z = 0
 if(sx == "C"):
   if(sy == 'C'):
     y = x
   elif(sy == 'F'):
     y = x*9/5+32
-
-z = 0
-if(sx == "C"):
-  elif(sz == "C"):
-    z = x
   elif(sz == 'K'):
     z = x+273.15
+
 st.write (x, ' ', sx, ' = ',y, sy)
 st.write (x, ' ', sx, ' = ',z, sz)
