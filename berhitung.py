e52f70ee7ab11ba81d8ff0ce1417899c41a1f2d9
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 11:01:34 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 x = st.number_input("Masukkan angka")
 sx = st.selectbox("Satuan", ("C", "F", "R", "K"), key='sx')
-st.write ("Anda memasukkan", x,' ',sx)
+st.write ("Anda memasukkan", x,'',sx)
 sy = st.selectbox("Dikonversi ke", ("C", "F", "R", "K"), key='sy')
 
 y = 0
+
 if sx == 'C':  # Celsius
     if sy == 'C':
         y = x
@@ -43,4 +44,4 @@ elif sx == 'R':  # Reaumur
     elif sy == 'R':
         y = x
 
-st.write (x,' ',sx, "=", y, sy)
+st.write(x, sx, "=", y, sy)
