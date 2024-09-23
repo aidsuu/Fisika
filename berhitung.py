072b46e5cabae120c825c74c30123a087dbc002d
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:31:07 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-sx = st.selectbox("sx:", ("C", "F"))
+sx = st.selectbox("Pilih Suhu", ("C", "F"))
+sy = st.selectbox("Pilih Suhu", ("C", "F"))
 
 
 
