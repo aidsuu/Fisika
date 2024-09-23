267290f7e3ffe792726a68696f2ba0d9da03df36
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:47:55 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-sx = st.selectbox("Satuan:", ("C", "F", "K", "R")
-sy = st.selecbox("Ke:", ("C", "F", "K", "R")
+sx = st.selectbox("Satuan:", ("C", "F", "K", "R"), key='sx')
+st.write = ("Anda memamsukkan", x,' ',sx)
+sy = st.selecbox("Dikonversi ke:", ("C", "F", "K", "R"), key='sy')
+y = 0
+if(sx == "C"):
+  if(sy == 'C'):
+    y = x
+  elif(sy == 'F'):
+    y = x*9/5+32
+st.write (x, ' ', sx, ' = ',y, sy)
+                 
 
