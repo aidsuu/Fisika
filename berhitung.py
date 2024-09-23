b4674dfa10e42ae8d74f6cb5475d93db3b96a2e1
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:55:13 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 x = st.number_input("Masukkan angka")
 sx = st.selectbox("Satuan", ("C", "F", "R", "K"), key='sx')
-st.write ("Anda memasukkan", x,'',sx)
+st.write ("Anda memasukkan", x,' ',sx)
 sy = st.selectbox("Dikonversi ke", ("C", "F", "R", "K"), key='sy')
 
 y = 0
