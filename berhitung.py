67623f14d288df338df31558fa8cd1e0b219d139
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-09-23 10:45:01 +0700

    Update berhitung.py

--- berhitung.py
+++ berhitung.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 x = st.number_input("Masukkan angka")
-sx = st.selectbox("Satuan", ("C", "F", "K", "R")
-sy = st.selecbox("Satuan", ("C", "F", "K", "R")
+sx = st.selectbox("Satuan:", ("C", "F", "K", "R")
+sy = st.selecbox("Ke:", ("C", "F", "K", "R")
 
