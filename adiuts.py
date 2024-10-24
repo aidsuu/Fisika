fbe500dfa15d7e606a7dcd7759c353229b664e93
aidsuu <146725349+aidsuu@users.noreply.github.com>
2024-10-24 08:57:00 +0700

    Create adiuts.py

---
+++ adiuts.py
@@ -0,0 +1,25 @@
+import numpy as np
+import mathplotlib.pylot as plt
+import streamlit as
+
+# Header
+st.header('Fisika Komputasi Awan')  
+st.subheader('Adi Saputra :sparkles:')
+
+# Membuat tombol untuk mengubah data acak
+if st.button('Data'):
+    # Membuat data acak
+    x = np.random.rand(50)
+    y = np.random.rand(50)
+    colors = np.random.rand(50)
+    area = (30 * np.random.rand(50))**2  # Ukuran bubble
+
+    # Membuat plot
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(x, y, s=area, c=colors, alpha=0.5)
+
+    # Menampilkan plot di Streamlit
+    st.pyplot(fig)
+
+# Keterangan
+st.caption('Data acak yang berubah setiap tombol ditekan')
